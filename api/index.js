283bb6efcf72ea4abdaca605057311fd362bef4f
Darshan Jain <darshanj.2610@gmail.com>
2024-08-14 15:23:34 +0530

    mongo connect

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.mongo)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 const app = express();
+
+
+
 
 app.listen(3000, () => {
     console.log(`server is  running on 3000 !`);
